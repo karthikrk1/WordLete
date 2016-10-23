21dde735b3c4eee95bd66155d7a6f008b92307d6
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2016-10-23 14:33:06 -0500

    Added new Launcher Activity

--- app/src/main/java/com/karthikrk/codesprintapp/LauncherActivity.java
+++ app/src/main/java/com/karthikrk/codesprintapp/LauncherActivity.java
@@ -1,7 +1,9 @@
 package com.karthikrk.codesprintapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+    }
+
+    public void onClickStart(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
